6e8fa17ff742db9d26c7c40c370b0c515fb57e1b
olopok <pabisa.pb@gmail.com>
2025-03-23 11:11:31 +0000

    Cleaned some files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
 import "../css/style.css";
-
-function revealTheMenu(x) {
-  x.classList.toggle("visible");
-}
-
-const element = document.querySelectorAll("button[type ='menu']");
-for (let i = 0; i < element.length; i++) {
-  element[i].addEventListener("click", (e) => {
-    const firstSibling = e.target.nextElementSibling;
-    revealTheMenu(firstSibling);
-  });
-}
