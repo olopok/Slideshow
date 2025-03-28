a7f4985b1d3dc62c6a95cc9442683bad7bb1a138
olopok <pabisa.pb@gmail.com>
2025-03-28 18:57:18 +0000

    Added images to the slide show in html and some function in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "../css/style.css";
+
+const next = document.querySelector("#next");
+const prev = document.querySelector("#previous");
+const slides = document.querySelectorAll(".slide");
+const circles = document.querySelectorAll(".circle");
+let slideIndex = 1;
+showSlide();
+// window.addEventListener("load", () => {
+//   showSlide(slideIndex);
+// });
+next.addEventListener("click", () => {
+  slideIndex += 1;
+  showSlide();
+});
+
+function showSlide() {
+  let i;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
