ae1dbbf714beb2315c9f9a6cea09df75e438b293
olopok <pabisa.pb@gmail.com>
2025-03-29 08:51:51 +0000

    Manual slide change done.

--- src/index.js
+++ src/index.js
@@ -5,19 +5,33 @@ const prev = document.querySelector("#previous");
 const slides = document.querySelectorAll(".slide");
 const circles = document.querySelectorAll(".circle");
 let slideIndex = 1;
-showSlide();
+showSlide(slideIndex);
 // window.addEventListener("load", () => {
 //   showSlide(slideIndex);
 // });
 next.addEventListener("click", () => {
   slideIndex += 1;
-  showSlide();
+  showSlide(slideIndex);
+});
+prev.addEventListener("click", () => {
+  slideIndex -= 1;
+  showSlide(slideIndex);
 });
 
-function showSlide() {
+function showSlide(n) {
   let i;
+  if (n === slides.length + 1) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  for (i = 0; i < circles.length; i++) {
+    circles[i].className = circles[i].className.replace(" active", "");
+  }
   slides[slideIndex - 1].style.display = "block";
+  circles[slideIndex - 1].className += " active";
 }
